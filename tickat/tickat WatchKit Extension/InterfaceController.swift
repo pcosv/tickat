9c50e89d90302065d35ceedb788ba9f049db05ec
Paula Vaz <pcosv@cin.ufpe.br>
2018-11-06 17:18:57 -0300

    project created

---
+++ tickat/tickat WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  tickat WatchKit Extension
+//
+//  Created by Paula Vaz on 06/11/18.
+//  Copyright © 2018 Paula Vaz. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ tickat/tickat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tickat
+//
+//  Created by Paula Vaz on 06/11/18.
+//  Copyright © 2018 Paula Vaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
