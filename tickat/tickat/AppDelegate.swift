32e930c173863ce34ae9d7a21672615ace0432e7
Lucas Cardoso <lccao@cin.ufpe.br>
2018-11-12 16:48:05 -0300

    Working and tested ViewControllerCuriosities

--- tickat/tickat/AppDelegate.swift
+++ tickat/tickat/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        AppData.shared.user.addCuriosity(newCuriosity: AppData.shared.blockedCuriosities[0])
         return true
     }
 
--- tickat/tickat/ViewControllerCuriosities.swift
+++ tickat/tickat/ViewControllerCuriosities.swift
@@ -14,18 +14,25 @@ class ViewControllerCuriosities: UIViewController, UITableViewDelegate, UITableV
     
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
+        curiositiesTableView.delegate = self
+        curiositiesTableView.dataSource = self
     }
     
+    // Vê a quantidade de itens na TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return AppData.shared.user.unblockedCuriosities.count
     }
     
+    
+    // Preenche os campos com as informações
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let tableCell:CuriositiesTableViewCell = tableView.dequeueReusableCell(withIdentifier: "curiositiesCell", for: indexPath) as! CuriositiesTableViewCell
         
-        tableCell.curiosityTitle.text = AppData.shared.user.unblockedCuriosities[indexPath.row].shortDescription
-        tableCell.curiosityText.text = AppData.shared.user.unblockedCuriosities[indexPath.row].longDescription
+        tableCell.curiosityTitle?.text = AppData.shared.user.unblockedCuriosities[indexPath.row].shortDescription
+        tableCell.curiosityText?.text = AppData.shared.user.unblockedCuriosities[indexPath.row].longDescription
+        self.curiositiesTableView.separatorStyle = .none
         
         return tableCell
     }
