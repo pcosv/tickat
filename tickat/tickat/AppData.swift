689c31ecaae06fe0e6683dbaeb670cff581e9d97
Lucas Cardoso <lccao@cin.ufpe.br>
2018-11-12 17:13:37 -0300

    Restructured AppData to work with location and first real one added

--- tickat/tickat/AppData.swift
+++ tickat/tickat/AppData.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 // Mediadora - dados do app
 // pra usar as variáveis, acessar por:   AppData.shared.variável
@@ -15,13 +16,13 @@ struct AppData {
     // usuário do app
     var user = User(id: "User")
     
-    // usuário fake pra teste
-//    var userTeste = User(id: "User Teste",
-//                         unblockedCuriosities: [Curiosity(isBlocked: false, shortDescription: "curta", longDescription: "longa", images: [UIImage(named: "check-box-empty")!])],
-//                         badges: [Badge(isBlocked: false, name: "badge teste", image: UIImage(named: "check-box-empty")!, curiositiesNeeded: 1)])
-    
-    
-    var blockedCuriosities:[Curiosity] = [Curiosity(isBlocked: true, shortDescription: "aa", longDescription: "AAA", images: [UIImage(named: "check-box-empty")!]), Curiosity(isBlocked: true, shortDescription: "bb", longDescription: "BBB", images: [UIImage(named: "check-box-empty")!])]
+    var allLocations: [Location] = [Location(isBlocked: true,
+                                             name: "Praça da Várzea",
+                                             curiosity: Curiosity(isBlocked: true,
+                                                                  shortDescription: "Essa praça rotineira quase recebeu um tapa no visual por Burle Marx, em 1936!",
+                                                                  longDescription: "Todo mundo que passa pela Praça Pinto Dâmaso nem imagina que, no passado, o paisagista Burle Marx fez um projeto pra ajardinar, colocar um playground, lago central e muita sombra - com mangueiras, oiti-da-praia, e palmeiras. A ideia dele era que virasse um ambiente massa pra galera se encontrar, mas infelizmente não foi executada completamente. Porém, se tu parar pra olhar, a praça tem traços do projeto, começando pelas palmeiras imperiais.",
+                                                                  images: [UIImage(named: "check-box-empty")!]),
+                                             coordinates: CLLocation(latitude: -8.048835, longitude: -34.959437))]
     
     var allBlockedBadges: [Badge] = [Badge(name: "Varzeano", image: UIImage(named: "check-box-empty")!, curiositiesNeeded: 5),
                                      Badge(name: "Recifense", image: UIImage(named: "check-box-empty")!, curiositiesNeeded: 15)]
