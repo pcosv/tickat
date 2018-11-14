bf010495e86909f3adfb6d60fed3b642d2f2e1ef
Paula Vaz <pcosv@cin.ufpe.br>
2018-11-14 15:21:31 -0300

    doing checkForProximity

--- tickat/tickat/ViewControllerMapCheckForProximity.swift
+++ tickat/tickat/ViewControllerMapCheckForProximity.swift
@@ -11,6 +11,7 @@ import CoreLocation
 
 extension ViewControllerMap{
     
+    // esta função pega a localização do dispositivo e compara com a de cada location do allLocations para desbloquear um novo local caso passe próximo a ele
     func checkForProximity() {
         var locManager = CLLocationManager()
         var currentLocation: CLLocation!
@@ -20,7 +21,18 @@ extension ViewControllerMap{
             
             currentLocation = locManager.location
             
+            print(currentLocation.coordinate.latitude)
+            print(currentLocation.coordinate.longitude)
         }
         
+        // varre o array verificando e desbloqueia o local e a curiosidade associada
+        for i in AppData.shared.allLocations{
+            //print(i.isBlocked)
+            if i.coordinates.coordinate.longitude == currentLocation.coordinate.longitude && i.coordinates.coordinate.latitude == currentLocation.coordinate.latitude{
+                i.isBlocked = false
+                //print(i.isBlocked)
+                i.curiosity.unblockCuriosity()
+            }
+        }
     }
 }
