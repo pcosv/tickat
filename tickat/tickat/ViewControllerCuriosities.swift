24e2d00c356eb2652843863b7b91d372b36ebd22
Camila Simões <camilasimoes65@gmail.com>
2018-11-14 15:02:44 -0300

    base do PopUp de curiosidades view, objetos linkados, abre e fecha alimentado pelo AppData

--- tickat/tickat/ViewControllerCuriosities.swift
+++ tickat/tickat/ViewControllerCuriosities.swift
@@ -12,6 +12,17 @@ class ViewControllerCuriosities: UIViewController, UITableViewDelegate, UITableV
     @IBOutlet weak var curiositiesTableView: UITableView!
     @IBOutlet weak var curiositiesLabel: UILabel!
     
+    //outlets do popup
+    @IBOutlet var popUpCuriosities: UIView!
+    @IBOutlet weak var imageCuriosityPopUp: UIImageView!
+    @IBOutlet weak var titleCuriosityPopUp: UILabel!
+    @IBOutlet weak var longDescriptionCuriosityPopUp: UILabel!
+    @IBAction func dismissPopUp(_ sender: Any) {
+        dismissPopUp(popUp: popUpCuriosities)
+    }
+
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,4 +47,36 @@ class ViewControllerCuriosities: UIViewController, UITableViewDelegate, UITableV
         
         return tableCell
     }
+    
+    // pra quando toca numa célula
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        imageCuriosityPopUp.image = AppData.shared.userTeste.unblockedCuriosities[indexPath.row].images[0]
+        titleCuriosityPopUp.text = AppData.shared.userTeste.unblockedCuriosities[indexPath.row].title
+        longDescriptionCuriosityPopUp.text = AppData.shared.userTeste.unblockedCuriosities[indexPath.row].longDescription
+        openPopUp(popUp: popUpCuriosities)
+    }
+    
+    
+    
+    
+    // popUp
+    func openPopUp(popUp: UIView) {
+        popUp.layer.cornerRadius = 20
+        popUp.layer.masksToBounds = true
+        popUp.center = self.view.center
+        // openBlur()
+        // self.view.isUserInteractionEnabled = false
+        self.view.addSubview(popUp)
+        
+    }
+    
+    
+    func dismissPopUp(popUp:UIView) {
+        popUp.removeFromSuperview()
+        // closeBlur()
+    }
+    
+    
+    
+    
 }
