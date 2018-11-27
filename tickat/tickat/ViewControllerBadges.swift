a4f177c24fe9e7c0e9990e7cae53925b6caa58bd
Lucas Cardoso <lccao@cin.ufpe.br>
2018-11-27 17:31:00 -0300

    Auto Layout fix

--- tickat/tickat/ViewControllerBadges.swift
+++ tickat/tickat/ViewControllerBadges.swift
@@ -46,6 +46,7 @@ class ViewControllerBadges: UIViewController, UICollectionViewDataSource, UIColl
             
             for subview in view.subviews{
                 if subview.isKind(of: UILabel.self){
+                    view.removeConstraints(subview.constraints)
                     subview.removeFromSuperview()
                 }
             }
@@ -61,7 +62,21 @@ class ViewControllerBadges: UIViewController, UICollectionViewDataSource, UIColl
 //            label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
 //            label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 
+            label.translatesAutoresizingMaskIntoConstraints = false
+            
             self.view.addSubview(label)
+            
+            self.view.addConstraints([NSLayoutConstraint(item: label, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1.0, constant: 0.8*self.view.frame.width),
+                                      NSLayoutConstraint(item: label, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: 64),
+                                      NSLayoutConstraint(item: label, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0),
+                                      NSLayoutConstraint(item: label, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0)])
+        } else {
+            for subview in view.subviews{
+                if subview.isKind(of: UILabel.self){
+                    view.removeConstraints(subview.constraints)
+                    subview.removeFromSuperview()
+                }
+            }
         }
         
         return badgesCount
